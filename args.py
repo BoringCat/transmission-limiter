da4d14db8eb1dfb023a0f478727e9b1cab298106
boringcat <boringcat@outlook.com>
2024-02-14 22:38:47 +0800

    修复Bug

--- args.py
+++ args.py
@@ -5,4 +5,4 @@ __log_level_map = logging.getLevelNamesMapping()
 LOG_LEVEL = __log_level_map.get(os.environ.get('LOG_LEVEL', 'INFO').upper(), logging.INFO)
 CONFIG_FILE = os.environ.get('CONFIG_FILE', '') or '/app/config/config.yml'
 
-__all__ = ['LOG_LEVEL'， 'CONFIG_FILE']
+__all__ = ['LOG_LEVEL', 'CONFIG_FILE']
